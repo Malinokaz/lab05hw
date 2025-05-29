ea12c3ef0f1db29b0f08e1279ba64ccfba78ca75
Malinokaz <98411157+Malinokaz@users.noreply.github.com>
2025-05-29 22:55:41 +0300

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -1,62 +1,113 @@
 #include <gtest/gtest.h>
-#include "Account.h"
+#include <gmock/gmock.h>
 #include "Transaction.h"
+#include "Account.h"
+
+using ::testing::Return;
+using ::testing::_;
+using ::testing::AtLeast;
+
+class MockAccount : public Account {
+public:
+    MockAccount(int id, int balance) : Account(id, balance) {}
+
+    MOCK_METHOD(int, GetBalance, (), (const, override));
+    MOCK_METHOD(void, ChangeBalance, (int), (override));
+    MOCK_METHOD(void, Lock, (), (override));
+    MOCK_METHOD(void, Unlock, (), (override));
+    MOCK_METHOD(int, id, (), (const));
+};
+
+class TestTransaction : public Transaction {
+public:
+    MOCK_METHOD(void, SaveToDataBase, (Account&, Account&, int), (override));
+};
 
 TEST(TransactionTest, SuccessTransferOver100) {
-    Account from(1, 1000);
-    Account to(2, 300);
-    Transaction t;
-    EXPECT_TRUE(t.Make(from, to, 200));
-    EXPECT_EQ(from.GetBalance(), 1000); 
-    EXPECT_EQ(to.GetBalance(), 300 + 200 - 201); 
+    MockAccount from(1, 1000);
+    MockAccount to(2, 300);
+    TestTransaction txn;
+
+    EXPECT_CALL(from, id()).WillRepeatedly(Return(1));
+    EXPECT_CALL(to, id()).WillRepeatedly(Return(2));
+
+    EXPECT_CALL(from, Lock());
+    EXPECT_CALL(to, Lock());
+    EXPECT_CALL(from, Unlock());
+    EXPECT_CALL(to, Unlock());
+
+    EXPECT_CALL(to, ChangeBalance(200));
+    EXPECT_CALL(to, GetBalance()).WillOnce(Return(500));
+    EXPECT_CALL(to, ChangeBalance(-201));
+
+    EXPECT_CALL(txn, SaveToDataBase(_, _, 200));
+
+    EXPECT_TRUE(txn.Make(from, to, 200));
 }
 
 TEST(TransactionTest, SameAccountThrows) {
-    Account acc(1, 1000);
-    Transaction t;
-    EXPECT_THROW(t.Make(acc, acc, 150), std::logic_error);
+    MockAccount acc(1, 1000);
+    TestTransaction txn;
+
+    EXPECT_CALL(acc, id()).WillRepeatedly(Return(1));
+    EXPECT_THROW(txn.Make(acc, acc, 150), std::logic_error);
 }
 
 TEST(TransactionTest, NegativeAmountThrows) {
-    Account from(1, 1000), to(2, 0);
-    Transaction t;
-    EXPECT_THROW(t.Make(from, to, -1), std::invalid_argument);
+    MockAccount from(1, 1000), to(2, 0);
+    TestTransaction txn;
+
+    EXPECT_CALL(from, id()).WillRepeatedly(Return(1));
+    EXPECT_CALL(to, id()).WillRepeatedly(Return(2));
+    EXPECT_THROW(txn.Make(from, to, -1), std::invalid_argument);
 }
 
 TEST(TransactionTest, TooSmallAmountThrows) {
-    Account from(1, 1000), to(2, 0);
-    Transaction t;
-    EXPECT_THROW(t.Make(from, to, 99), std::logic_error);
+    MockAccount from(1, 1000), to(2, 0);
+    TestTransaction txn;
+
+    EXPECT_CALL(from, id()).WillRepeatedly(Return(1));
+    EXPECT_CALL(to, id()).WillRepeatedly(Return(2));
+    EXPECT_THROW(txn.Make(from, to, 99), std::logic_error);
 }
 
 TEST(TransactionTest, FeeTooBigReturnsFalse) {
-    Account from(1, 1000), to(2, 0);
-    Transaction t;
-    EXPECT_FALSE(t.Make(from, to, 1));  
+    MockAccount from(1, 1000), to(2, 0);
+    TestTransaction txn;
+    txn.set_fee(100);
+
+    EXPECT_CALL(from, id()).WillRepeatedly(Return(1));
+    EXPECT_CALL(to, id()).WillRepeatedly(Return(2));
+    EXPECT_FALSE(txn.Make(from, to, 100));
 }
 
 TEST(TransactionTest, InsufficientFundsInFromReturnsFalse) {
-    Account from(1, 100);
-    Account to(2, 0);
-    Transaction t;
-    EXPECT_FALSE(t.Make(from, to, 100)); 
-}
+    MockAccount from(1, 100), to(2, 0);
+    TestTransaction txn;
 
+    EXPECT_CALL(from, id()).WillRepeatedly(Return(1));
+    EXPECT_CALL(to, id()).WillRepeatedly(Return(2));
+    EXPECT_FALSE(txn.Make(from, to, 100));
+}
 
 TEST(TransactionTest, RollbackOnDebitFailure) {
-    Account from(1, 1000);
-    Account to(2, 0); 
-    Transaction t;
-    EXPECT_FALSE(t.Make(from, to, 1100));  
-    EXPECT_EQ(from.GetBalance(), 1000);    
-    EXPECT_EQ(to.GetBalance(), 0);        
-}
+    MockAccount from(1, 1000);
+    MockAccount to(2, 0);
+    TestTransaction txn;
 
-TEST(TransactionTest, AllBranchesInMakeCovered) {
-    Account a1(1, 500);
-    Account a2(2, 0);
-    Transaction t;
-    EXPECT_FALSE(t.Make(a1, a2, 1));    // fee*2 > sum
-    EXPECT_THROW(t.Make(a1, a2, -1), std::invalid_argument); // negative
-    EXPECT_THROW(t.Make(a1, a2, 99), std::logic_error);      // too small
+    EXPECT_CALL(from, id()).WillRepeatedly(Return(1));
+    EXPECT_CALL(to, id()).WillRepeatedly(Return(2));
+
+    EXPECT_CALL(from, Lock());
+    EXPECT_CALL(to, Lock());
+    EXPECT_CALL(from, Unlock());
+    EXPECT_CALL(to, Unlock());
+
+    EXPECT_CALL(to, ChangeBalance(1100));
+    EXPECT_CALL(to, GetBalance()).WillOnce(Return(0));
+    EXPECT_CALL(to, ChangeBalance(-1100));
+
+    EXPECT_CALL(txn, SaveToDataBase(_, _, 1100));
+
+    EXPECT_FALSE(txn.Make(from, to, 1100));
 }
